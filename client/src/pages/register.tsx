7a675de4b42d4c3d22ffe614e88a3b8ddc098ee0
Farrukh Jamal <farrukhjamal806@gmail.com>
2022-12-19 14:13:17 +0530

    Urql graphql added

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { Form, Formik } from 'formik';
 import { Box, Button } from '@chakra-ui/react';
-import { useMutation } from 'urql';
 
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
-
-import REGISTER_MUTATION from '../GraphQL/registerMutation';
+import { useRegisterMutation } from '../gql/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = () => {
-	const [_, register] = useMutation(REGISTER_MUTATION);
+	const [_, register] = useRegisterMutation();
+	const router = useRouter();
 	return (
 		<Wrapper>
 			<Formik
 				initialValues={{ username: '', password: '' }}
-				onSubmit={(values) => {
-					console.log(values);
-					return register(values);
+				onSubmit={async (values, { setErrors }) => {
+					const response = await register(values);
+					if (response.data?.register.errors) {
+						setErrors(toErrorMap(response.data.register.errors));
+					} else if (response.data?.register.user) {
+						// user register works
+						router.push('/');
+					}
 				}}>
 				{({ isSubmitting }) => (
 					<Form>
